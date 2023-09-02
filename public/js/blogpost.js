5858b68a771061f2f6daa4d37b6c243fc771392d
Abe Omaits <aomaits@gmail.com>
2023-09-02 08:22:59 -0500

    worked on dashboard routing

--- public/js/blogpost.js
+++ public/js/blogpost.js
@@ -1,6 +1,3 @@
-// add event listener! 
-// need to be able to add comments
-
 const commentFormHandler = async (event) => {
     event.preventDefault();
     const commentBody = document.querySelector('#comment-input').value.trim();
@@ -25,35 +22,7 @@ const commentFormHandler = async (event) => {
     }
 }
 
-// function for the sign up form. User alerted if sign up works or fails
-// const signupFormHandler = async (event) => {
-//     console.log('signup form handler running')
-//     event.preventDefault();
-//     const user_name = document.querySelector('#name-signup').value.trim();
-//     console.log("this is the New user_name: " + user_name);
-//     const password = document.querySelector('#password-signup').value.trim();
-//     console.log("this is the password: " + password);
-
-//     if (user_name && password) {
-//         const response = await fetch('/api/users', {
-//             method: 'POST',
-//             body: JSON.stringify({ user_name, password }),
-//             headers: { 'Content-Type': 'application/json' },
-//         });
-//         if (response.ok) {
-//             alert('Congratulations, ' + user_name + '! You are now signed up with the Tech Blog!');
-//         } else {
-//             alert('Please make your password at least 8 characters long.');
-//         }
-//     }
-// };
-
 // listen for click of comment button 
 document
     .querySelector('#comment-button')
     .addEventListener('click', commentFormHandler);
-
-// submit button created for sign up
-// document
-//     .querySelector('.signup-form')
-//     .addEventListener('submit', signupFormHandler);
